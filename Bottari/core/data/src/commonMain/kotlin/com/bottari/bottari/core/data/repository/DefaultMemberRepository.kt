b4c7f22430c878e4ad95b724f5775c74a3ef4899
moondev03 <moondev03@gmail.com>
2026-02-12 06:36:34 +0900

    feat(data): add member repository and DI bindings

---
+++ Bottari/core/data/src/commonMain/kotlin/com/bottari/bottari/core/data/repository/DefaultMemberRepository.kt
@@ -0,0 +1,39 @@
+package com.bottari.bottari.core.data.repository
+
+import com.bottari.bottari.core.di.DataScope
+import com.bottari.bottari.core.domain.repository.MemberRepository
+import com.bottari.bottari.core.model.member.Nickname
+import com.bottari.bottari.core.model.member.RegisteredMember
+import com.bottari.bottari.core.network.datasource.MemberRemoteDataSource
+import com.bottari.bottari.core.network.model.member.MemberNicknameSaveRequest
+import com.bottari.bottari.core.network.model.member.MemberRegisterRequest
+import com.bottari.bottari.core.network.util.FcmTokenProvider
+import com.bottari.bottari.core.network.util.FirebaseInstallationIdProvider
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.SingleIn
+
+@Inject
+@SingleIn(DataScope::class)
+class DefaultMemberRepository(
+    private val dataSource: MemberRemoteDataSource,
+    private val fidProvider: FirebaseInstallationIdProvider,
+    private val fcmTokenProvider: FcmTokenProvider,
+) : MemberRepository {
+    override suspend fun registerMember(): Result<Long?> {
+        val fid = fidProvider.getInstallationId()
+        val fcmToken = fcmTokenProvider.getToken()
+        return dataSource.registerMember(
+            MemberRegisterRequest(
+                ssaid = fid,
+                fcmToken = fcmToken,
+            ),
+        )
+    }
+
+    override suspend fun saveMemberNickname(nickname: Nickname): Result<Unit> =
+        dataSource.saveMemberNickname(MemberNicknameSaveRequest.fromDomain(nickname))
+
+    override suspend fun checkRegisteredMember(): Result<RegisteredMember> =
+        dataSource.checkRegisteredMember()
+            .map { response -> response.toDomain() }
+}
